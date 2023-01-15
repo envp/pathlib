ec1fda8a8379554ae463160159f55ab21e88afcf
Vaibhav Yenamandra <3663231+envp@users.noreply.github.com>
2023-01-15 13:07:22 -0500

    lib+tests: Implement basename, suffix methods.
    Add test cases as well.

--- src/include/pathlib/pure_path.hpp
+++ src/include/pathlib/pure_path.hpp
@@ -2,9 +2,10 @@
 #define PATHLIB_PURE_PATH_HPP
 
 #include "pathlib/annotations.hpp"
+#include "pathlib/assertions.hpp"
 #include "pathlib/utils/string_utils.hpp"
 
-#include <cassert>
+#include <algorithm>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -30,12 +31,35 @@ public:
   }
 
   /// Return the filename component of the path. The contained
-  PATHLIB_NODISCARD std::optional<std::string_view> name() const {
+  PATHLIB_NODISCARD std::optional<std::string_view> basename() const {
     auto *path_flavour = static_cast<const Flavour *>(this);
     if (path_flavour->is_root()) {
       return std::nullopt;
     }
-    return path_flavour->d_parts.back();
+    const auto &components = parts();
+    if (components.empty()) {
+      return std::nullopt;
+    }
+    std::string_view name = components.back();
+    if (name == "..") {
+      return std::nullopt;
+    }
+    return name;
+  }
+
+  /// Return the suffix part of the filename, if any.
+  PATHLIB_NODISCARD std::optional<std::string_view> suffix() const {
+    std::optional<std::string_view> name = this->basename();
+    // std::function<bool(std::string_view)> func = &std::string_view::empty;
+    if (!name || name->empty()) {
+      return std::nullopt;
+    }
+    auto dotpos = name->rfind('.');
+    if (0 == dotpos || std::string_view::npos == dotpos) {
+      return std::nullopt;
+    }
+    name->remove_prefix(dotpos);
+    return name;
   }
 };
 
@@ -53,7 +77,7 @@ public:
   PATHLIB_NODISCARD const std::vector<std::string> &parts() const;
 
 private:
-  static const char PRIMARY_SEPARATOR = '/';
+  static constexpr char PRIMARY_SEPARATOR = '/';
   bool d_is_absolute;
   std::vector<std::string> d_parts;
 };
--- tests/001_pure_posix_path.t.cpp
+++ tests/001_pure_posix_path.t.cpp
@@ -75,6 +75,10 @@ TEST(PurePosixPathTest, AbsolutePaths) {
   EXPECT_TRUE(PurePosixPath("/./././").is_absolute());
   EXPECT_TRUE(PurePosixPath("/..").is_absolute());
   EXPECT_TRUE(PurePosixPath("/a/").is_absolute());
+  EXPECT_TRUE(PurePosixPath("//a/").is_absolute());
+
+  // This is somewhat non-sensical, in that it is unresolvable. but consistent?
+  EXPECT_TRUE(PurePosixPath("/..").is_absolute());
 }
 
 TEST(PurePosixPathTest, RelativePaths) {
@@ -82,4 +86,48 @@ TEST(PurePosixPathTest, RelativePaths) {
   EXPECT_FALSE(PurePosixPath("./././").is_absolute());
   EXPECT_FALSE(PurePosixPath("..").is_absolute());
   EXPECT_FALSE(PurePosixPath("a/").is_absolute());
+}
+
+TEST(PurePosixPathTest, HasNoBasename) {
+  EXPECT_FALSE(PurePosixPath("").basename().has_value());
+  EXPECT_FALSE(PurePosixPath("/").basename().has_value());
+  EXPECT_FALSE(PurePosixPath(".").basename().has_value());
+  EXPECT_FALSE(PurePosixPath("..").basename().has_value());
+}
+
+TEST(PurePosixPathTest, HasBasename) {
+  EXPECT_EQ(PurePosixPath("foo").basename(), "foo");
+  EXPECT_EQ(PurePosixPath("./foo").basename(), "foo");
+  EXPECT_EQ(PurePosixPath("relative/path/to/foo").basename(), "foo");
+  EXPECT_EQ(PurePosixPath("./relative/path/to/foo").basename(), "foo");
+  EXPECT_EQ(PurePosixPath("../relative/path/to/foo").basename(), "foo");
+  EXPECT_EQ(PurePosixPath("/absolute/path/to/foo").basename(), "foo");
+
+  EXPECT_EQ(PurePosixPath("foo.tar").basename(), "foo.tar");
+  EXPECT_EQ(PurePosixPath("./foo.tar").basename(), "foo.tar");
+  EXPECT_EQ(PurePosixPath("relative/path/to/foo.tar").basename(), "foo.tar");
+  EXPECT_EQ(PurePosixPath("./relative/path/to/foo.tar").basename(), "foo.tar");
+  EXPECT_EQ(PurePosixPath("../relative/path/to/foo.tar").basename(), "foo.tar");
+  EXPECT_EQ(PurePosixPath("/absolute/path/to/foo.tar").basename(), "foo.tar");
+
+  EXPECT_EQ(PurePosixPath("foo.tar.").basename(), "foo.tar.");
+  EXPECT_EQ(PurePosixPath("foo.tar.gz").basename(), "foo.tar.gz");
+}
+
+TEST(PurePosixPathTest, HasNoSuffix) {
+  EXPECT_FALSE(PurePosixPath("").suffix().has_value());
+  EXPECT_FALSE(PurePosixPath(".").suffix().has_value());
+  EXPECT_FALSE(PurePosixPath("..").suffix().has_value());
+  EXPECT_FALSE(PurePosixPath("/").suffix().has_value());
+  EXPECT_FALSE(PurePosixPath("/foo").suffix().has_value());
+  EXPECT_FALSE(PurePosixPath("/foo/bar/.").suffix().has_value());
+  EXPECT_FALSE(PurePosixPath("foo/bar/.baz").suffix().has_value());
+  EXPECT_FALSE(PurePosixPath("foo/bar/baz").suffix().has_value());
+}
+
+TEST(PurePosixPathTest, HasSuffix) {
+  EXPECT_TRUE(PurePosixPath("/foo/bar/baz.tar").suffix().has_value());
+  EXPECT_EQ(PurePosixPath("/foo/bar/baz.tar").suffix(), ".tar");
+  EXPECT_EQ(PurePosixPath("/foo/bar/baz.tar.gz").suffix(), ".gz");
+  EXPECT_EQ(PurePosixPath("foo.tar.").suffix(), ".");
 }
